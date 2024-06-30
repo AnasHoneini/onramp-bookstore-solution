bc2b196c9bb65997813df8038ad4b0a372319a90
AnasHoneini <anas.hneineh@gmail.com>
2024-06-30 18:01:49 +0300

    Fix code style issues with Prettier

--- tests/deleteBook.test.ts
+++ tests/deleteBook.test.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import { app, server } from '../server';
 
-afterAll(() => {
-  server.close();
+afterAll((done) => {
+  server.close(done);
 });
 
 describe('DELETE /api/books/:id', () => {
--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,24 +1,20 @@
 import request from 'supertest';
 import { app, server } from '../server';
 
-type Book = {
-  id: number;
-  title: string;
-  author: string;
-};
-
 afterAll((done) => {
   server.close(done);
 });
 
-describe('Get /api/books', () => {
+describe('GET /api/books', () => {
   it('should return a list of books', async () => {
     const response = await request(app).get('/api/books');
     expect(response.status).toBe(200);
-    response.body.forEach((book: Book) => {
-      expect(book).toHaveProperty('id');
-      expect(book).toHaveProperty('title');
-      expect(book).toHaveProperty('author');
-    });
+    response.body.forEach(
+      (book: { id: number; title: string; author: string }) => {
+        expect(book).toHaveProperty('id');
+        expect(book).toHaveProperty('title');
+        expect(book).toHaveProperty('author');
+      },
+    );
   });
 });
